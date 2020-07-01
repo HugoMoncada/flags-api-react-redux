8f78ebfbe1e27c9d2d5cada9ddd52410e1a91729
HugoMoncada <hugo_moncada@outlook.com>
2020-07-01 12:42:10 -0500

    Dark mode as future feature everything else ready!

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,12 +8,12 @@ const NavBar = () => {
                     
                   <Link className="navbar-brand py-3" to="/"><h3><strong>Where in the world?</strong></h3></Link>
                     
-                  <button className="btn btn-outline-dark my-3 my-sm-0 mr-3" type="submit">
+                  {/* <button className="btn btn-outline-dark my-3 my-sm-0 mr-3" type="submit">
                     <svg width="1em" height="1em" viewBox="0 0 20 20" className="bi bi-moon" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fillRule="evenodd" d="M14.53 10.53a7 7 0 0 1-9.058-9.058A7.003 7.003 0 0 0 8 15a7.002 7.002 0 0 0 6.53-4.47z"/>
                     </svg>
                     Dark mode
-                  </button>
+                  </button> */}
                 </div>
              </nav>
      )
